366f569c2188bf5b2ea81f0178b33a50d57ae3b2
Noah Johnson <noahjohnson15.nj@gmail.com>
2018-05-01 15:55:09 -0400

    added try/catch

--- object_detection/object_detector.py
+++ object_detection/object_detector.py
@@ -107,19 +107,22 @@ if __name__ == '__main__':
     video_capture = WebcamVideoStream(src=args.video_source, width=args.width, height=args.height).start()
     fps = FPS().start()
 
-    while True:  # fps._numFrames < 120
-        frame = video_capture.read()
-        input_q.put(frame)
+    try:
+        while True:  # fps._numFrames < 120
+            frame = video_capture.read()
+            input_q.put(frame)
 
-        t = time.time()
-        #output_rgb = cv2.cvtColor(output_q.get(), cv2.COLOR_RGB2BGR)
-        cv2.imshow('Video', output_q.get())
-        fps.update()
+            t = time.time()
+            #output_rgb = cv2.cvtColor(output_q.get(), cv2.COLOR_RGB2BGR)
+            cv2.imshow('Video', output_q.get())
+            fps.update()
 
-        print('[INFO] elapsed time: {:.2f}'.format(time.time() - t))
+            print('[INFO] elapsed time: {:.2f}'.format(time.time() - t))
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+            #if cv2.waitKey(1) & 0xFF == ord('q'):
+            #    break
+    except KeyboardInterrupt:
+        fps.stop()
 
     fps.stop()
     print('[INFO] elapsed time (total): {:.2f}'.format(fps.elapsed()))
